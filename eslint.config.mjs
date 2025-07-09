59d8f526a7e514d463c3c733375660987b7de622
iamkuldeep07 <kuldeep45.kd@gmail.com>
2025-07-10 04:57:01 +0530

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,18 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  // ✅ Next.js config
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // ✅ Ignore Prisma-generated files (or override rules)
+  {
+    files: ["lib/generated/prisma/**/*", "node_modules/@prisma/client/**/*"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unnecessary-type-constraint": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
